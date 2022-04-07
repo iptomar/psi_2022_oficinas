253c54e5f48fba02aaca178576c025b2705d6250
dplt1992 <aluno19169@ipt.pt>
2022-04-07 15:55:52 +0100

    Primeiro commit. Projeto criado.

---
+++ psi_2022_oficinas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace psi_2022_oficinas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
