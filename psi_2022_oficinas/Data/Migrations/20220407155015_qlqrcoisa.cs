f7db9835e1843855b4167cfa27e645317cf62faf
RexCaelum <aluno22113@ipt.pt>
2022-04-07 17:00:51 +0100

    tab cliente

---
+++ psi_2022_oficinas/Data/Migrations/20220407155015_qlqrcoisa.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace psi_2022_oficinas.Data.Migrations
+{
+    public partial class qlqrcoisa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- psi_2022_oficinas/Models/Clientes.cs
+++ psi_2022_oficinas/Models/Clientes.cs
@@ -2,6 +2,15 @@
 {
     public class Clientes
     {
-       
+       public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int NIF { get; set; }
+        public int CConducao { get; set; }
+        public string Morada { get; set; }
+        public int Ntele { get; set; }
+
+
     }
+
 }
